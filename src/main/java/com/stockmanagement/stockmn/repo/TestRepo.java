816b2025e6230f38b6a4dc60e57a101b7b8efc54
Ruwin Dissanayake <147265928+ruwidissa@users.noreply.github.com>
2024-06-23 11:46:21 +0530

    Delete src directory

--- src/main/java/com/stockmanagement/stockmn/repo/TestRepo.java
+++
@@ -1,7 +0,0 @@
-package com.stockmanagement.stockmn.repo;
-
-import com.stockmanagement.stockmn.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepo extends JpaRepository<Test, Integer> {
-}
--- src/src/main/java/com/stockmanagement/stockmn/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.stockmanagement.stockmn.repo;
-
-import com.stockmanagement.stockmn.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface
-UserRepo extends JpaRepository<User, Integer> {
-}
